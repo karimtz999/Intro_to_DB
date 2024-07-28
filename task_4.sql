3cb3fe40077331b6b752ce372747dd9c5d665e82
ELFA <140125432+karimtz999@users.noreply.github.com>
2024-07-28 21:14:55 +0100

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,8 @@
+-- Use the alx_book_store database
 USE alx_book_store;
 
+-- Retrieve the column information for the books table
 SELECT COLUMN_NAME, COLUMN_TYPE, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_NAME = 'Books';
+
