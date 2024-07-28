4de16cf6c52b46aad5478c49ed9d2e27040ed29b
ELFA <140125432+karimtz999@users.noreply.github.com>
2024-07-28 21:20:39 +0100

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,13 +1,18 @@
 SELECT
-    COLUMN_NAME,
-    COLUMN_TYPE,
-    IS_NULLABLE,
-    COLUMN_KEY,
-    COLUMN_DEFAULT,
-    EXTRA
+    columns.TABLE_NAME,
+    columns.COLUMN_NAME,
+    columns.COLUMN_TYPE,
+    columns.IS_NULLABLE,
+    columns.COLUMN_DEFAULT,
+    columns.COLUMN_KEY,
+    columns.EXTRA
 FROM
-    information_schema.COLUMNS
+    INFORMATION_SCHEMA.COLUMNS AS columns
+JOIN
+    INFORMATION_SCHEMA.TABLES AS tables
+ON
+    columns.TABLE_SCHEMA = tables.TABLE_SCHEMA
+    AND columns.TABLE_NAME = tables.TABLE_NAME
 WHERE
-    TABLE_SCHEMA = 'alx_book_store'
-    AND TABLE_NAME = 'books';
-
+    tables.TABLE_SCHEMA = 'alx_book_store'
+    AND tables.TABLE_NAME = 'books';
