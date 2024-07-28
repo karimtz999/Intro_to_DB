8d8b939572714c775a39e781addca3c445fd4d80
ELFA <140125432+karimtz999@users.noreply.github.com>
2024-07-28 21:25:00 +0100

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,5 +1,3 @@
-USE alx_book_store;
 
-SELECT COLUMN_NAME, COLUMN_TYPE, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'Books';
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
