e2669a74dfed2b8949f70b3db9bde3a2383a8ea7
ELFA <140125432+karimtz999@users.noreply.github.com>
2024-07-28 21:21:25 +0100

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,18 +1,17 @@
+
 SELECT
-    columns.TABLE_NAME,
     columns.COLUMN_NAME,
     columns.COLUMN_TYPE,
     columns.IS_NULLABLE,
     columns.COLUMN_DEFAULT,
-    columns.COLUMN_KEY,
     columns.EXTRA
 FROM
     INFORMATION_SCHEMA.COLUMNS AS columns
-JOIN
-    INFORMATION_SCHEMA.TABLES AS tables
-ON
-    columns.TABLE_SCHEMA = tables.TABLE_SCHEMA
-    AND columns.TABLE_NAME = tables.TABLE_NAME
 WHERE
-    tables.TABLE_SCHEMA = 'alx_book_store'
-    AND tables.TABLE_NAME = 'books';
+    columns.TABLE_SCHEMA = 'alx_book_store'
+    AND columns.TABLE_NAME IN (
+        SELECT TABLE_NAME
+        FROM INFORMATION_SCHEMA.TABLES
+        WHERE TABLE_SCHEMA = 'alx_book_store'
+        AND TABLE_NAME LIKE 'books%'
+    );
